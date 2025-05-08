c2e5637893af22038c377a0ece23ba050e031558
Nikhil Yates <nikhil.yates@outlook.com>
2025-05-08 18:08:48 -0400

    about update test

--- scorpionlabz-v2/src/app/about/page.tsx
+++ scorpionlabz-v2/src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function About() {
-  return <div className="h-full w-full flex flex-col relative justify-top gap-[25%] overflow-hidden">About</div>;
+  return <div className="h-full w-full flex flex-col relative justify-top gap-[25%] overflow-hidden">About Section</div>;
 }
