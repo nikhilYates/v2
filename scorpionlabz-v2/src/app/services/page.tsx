fea0a6a8c7d1ca92703e19fa226520f8f3634d0a
Nikhil Yates <nikhil.yates@outlook.com>
2025-05-10 20:24:32 -0400

    services is in

--- scorpionlabz-v2/src/app/services/page.tsx
+++ scorpionlabz-v2/src/app/services/page.tsx
@@ -20,8 +20,11 @@ export default function Services() {
     }
 
   return (
-    <div className="w-full flex flex-col h-screen justify-start gap-4 items-center p-8 2xl:p-16 bg-black relative">
+    <div className="w-full flex flex-col h-auto justify-start gap-4 items-center p-8 2xl:p-16 bg-black relative pb-8">
         <h1 className="text-white text-4xl 2xl:text-7xl font-regular text-center">What We Do</h1>
+        <p className="text-white text-xl font-medium text-center w-full 2xl:w-1/2 relative z-10">
+          We do what we do, and we do it well. Each project is treated with the same level of attention and crafsmanship - our signature is quality.
+        </p>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-16">
         {servicesData.services.map((service, index) => (
             <ServiceCard key={index} service={service} />
